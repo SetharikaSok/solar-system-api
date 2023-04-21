159c20a55b13b0bf9ddedabf68d8222a77fe221d
Amanda <jonesamanda996@gmail.com>
2023-04-21 11:13:08 -0400

    wave 1

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,17 @@
 from flask import Blueprint
 
+class Planet():
+    def __init__ (self, id, name, description, color):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.color = color
+
+
+planets = [
+    Plant(1, "Mars", "Similar to Earth", "Copper"),
+    Plant(2, "Neptune", "Farthest planet in solar system", "Blue"),
+    Plant(3, "Earth", "Where life is known to exist", "Blue-Green"),
+    ]
+
+planet_bp = Blueprint("planets", __name__, url_prefix="/planets")
