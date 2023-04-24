5d683825bd08676db259eae19d6be78aeedca3fe
Amanda <jonesamanda996@gmail.com>
2023-04-24 14:59:18 -0400

    wave 2

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet():
     def __init__ (self, id, name, description, color):
@@ -14,10 +14,20 @@ planets = [
     Planet(3, "Earth", "Where life is known to exist", "Blue-Green"),
     ]
 
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message": f"planet {planet_id} invalid"}, 400))
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+    abort(make_response({"message": f"planet {planet_id} not found"}, 404))
+
 planet_bp = Blueprint("planets", __name__, url_prefix="/planets")
 @planet_bp.route("", methods = ["GET"])
 
-def handle_planet():
+def handle_planets():
     planet_response= []
     for planet in planets:
         planet_response.append({
@@ -29,3 +39,13 @@ def handle_planet():
         })
 
     return jsonify(planet_response)
+
+@planet_bp.route("/<planet_id>", methods = ["GET"])
+def handle_planet(planet_id):
+    planet = validate_planet(planet_id)
+    return {
+            "id":planet.id,
+            "name":planet.name,
+            "description":planet.description,
+            "color":planet.color
+        }
